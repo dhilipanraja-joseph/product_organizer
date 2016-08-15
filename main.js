dabf0bc9bcff447888612928dc0088e673377e1e
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-08-15 10:04:44 -0700

    add createProduct

--- main.js
+++ main.js
@@ -1,37 +1,100 @@
 const Root = React.createClass({
+  getInitialState(){
+    try{
+      var data=JSON.parse(localStorage.products);
+    }catch(e){
+      data = [];
+    }
+    return{
+      products : data
+    }
+  },
+  componentDidUpdate(){
+
+      localStorage.products = JSON.stringify(this.state.products);
+  },
+  addProduct(product) {
+
+    console.log("add product",product);
+
+      this.setState({products : this.state.products.concat(product)});
+
+  },
+  deleteProduct(id){
+
+    this.setState({products : this.state.products.filter(product => product.id !== id)});
+  },
+  modifyProduct(id){
+
+  },
   render(){
     return (
       <div>
         <h1>Product Organizer</h1>
-        <CreateProduct/>
+        <ProductForm addProduct={this.addProduct}/>
       </div>
     );
   }
 });
 
-const CreateProduct = React.createClass({
+
+
+
+
+
+const ProductForm = React.createClass({
   getInitialState(){
     return {
       name : '',
       price : '',
-      descr : ''
+      descr : '',
+      imgurl : ''
     }
   },
-  resetForm(){
-    
+  resetForm(e){
+    e.preventDefault();
+    this.setState({
+      name : '',
+      price : '',
+      descr : '',
+      imgurl : ''
+    });
+  },
+  addProduct(e){
+    e.preventDefault();
+    let product = {
+      name : this.state.name,
+      price : this.state.price,
+      descr : this.state.descr,
+      imgurl : this.state.imgurl
+    };
+    this.props.addProduct(product);
+    this.setState({
+      name : '',
+      price : '',
+      descr : '',
+      imgurl : ''
+    });
   },
   render(){
     return (
-      <form>
+      <form onSubmit={this.addProduct}>
         <input type="text" value={this.state.name}  onChange={e=>this.setState({name:e.target.value})} placeholder="Product Name"/><br/>
-        <input type="text" value={this.state.price}  onChange={e=>this.setState({price:e.target.value})} placeholder="Price"/><br/>
+        <input type="number" value={this.state.price}  onChange={e=>this.setState({price:e.target.value})} placeholder="Price"/><br/>
         <input type="text" value={this.state.descr}  onChange={e=>this.setState({descr:e.target.value})} placeholder="Description"/><br/>
+        <input type="text" value={this.state.imgurl}  onChange={e=>this.setState({imgurl:e.target.value})} placeholder="img link"/><br/>
         <button type="submit">Add</button>
-        <button onclick={this.resetForm}>Reset</button>
+        <button onClick={this.resetForm}>Reset</button>
       </form>
     );
   }
 });
+
+
+
+
+
+
 
 ReactDOM.render(
   <div>
